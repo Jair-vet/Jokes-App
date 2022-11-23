0d97ffa6fdcdb8932816017c0c393b77d4b2adf4
Jair <jairaceves56@gmail.com>
2022-11-23 08:38:09 -0600

    Joke's app

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
-const panels = document.querySelectorAll('.panel')
+const jokeEl = document.getElementById('joke')
+const jokeBtn = document.getElementById('jokeBtn')
 
-panels.forEach( panel => {
-    panel.addEventListener('click', () => {
-        removeActiveClasses()
-        panel.classList.add('active')
-    })
-})
+jokeBtn.addEventListener('click', generateJoke)
 
-function removeActiveClasses() {
-    panels.forEach( panel => {
-        panel.classList.remove('active')
-    })
+generateJoke()
+
+// USING ASYNC/AWAIT
+async function generateJoke() {
+  const config = {
+    headers: {
+      Accept: 'application/json',
+    },
+  }
+
+  const res = await fetch('https://icanhazdadjoke.com', config)
+
+  const data = await res.json()
+
+  jokeEl.innerHTML = data.joke
 }
 
+// USING .then()
+// function generateJoke() {
+//   const config = {
+//     headers: {
+//       Accept: 'application/json',
+//     },
+//   }
+
+//   fetch('https://icanhazdadjoke.com', config)
+//     .then((res) => res.json())
+//     .then((data) => {
+//       jokeEl.innerHTML = data.joke
+//     })
+// }
